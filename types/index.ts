bcd9ba8f9f02d14f75fc8ecd6008058a4fc1f894
Hideaki-Ryu <cenarahmant@gmail.com>
2023-10-10 09:03:21 +0700

    feat: add AuthDaoInterface type

--- types/index.ts
+++ types/index.ts
@@ -4,3 +4,8 @@ export interface BookDaoInterface {
   updateBook(id: string, name: string, author: string): Promise<any>;
   deleteBook(id: string): Promise<any>;
 }
+
+export interface AuthDaoInterface {
+  loginUser(userInfo: { username: string; password: string }): Promise<any>;
+  registerUser(userInfo: { username: string; password: string }): Promise<any>;
+}
