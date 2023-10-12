3b4ef5acd65d6414ce04fdce37c6d5880af1ba89
Hideaki-Ryu <cenarahmant@gmail.com>
2023-10-12 23:52:35 +0700

    chore: modif morgan to dev

--- middleware/morganMiddleware.ts
+++ middleware/morganMiddleware.ts
@@ -3,10 +3,12 @@ import { Application } from "express";
 import morgan from "morgan";
 
 const morganMiddleware = (app: Application) => {
+  // Set log to reuest.log file
   // const accessLogStream = fs.createWriteStream("request.log", {
   //   flags: "a",
   // });
   // const logger = morgan("combined", { stream: accessLogStream });
+  // app.use(logger);
   app.use(morgan("dev"));
 };
 
